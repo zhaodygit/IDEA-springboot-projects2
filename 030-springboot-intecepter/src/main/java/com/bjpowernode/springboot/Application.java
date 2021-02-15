62abec0f141a95cf8ee05f600d9caf5f31da7cf0
zhaod <zhaod@DESKTOP-QMI0R5J>
2021-02-15 22:33:52 +0800

    token

--- 030-springboot-intecepter/src/main/java/com/bjpowernode/springboot/Application.java
+++ 030-springboot-intecepter/src/main/java/com/bjpowernode/springboot/Application.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+    /**
+     * 启动位置
+     * 9ad675029a802bd0588556337041a403353980b1
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
